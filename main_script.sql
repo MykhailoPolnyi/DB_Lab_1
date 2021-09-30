098498090e4da917f8fd94df20d286625153baf2
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-09-30 04:51:36 +0300

    Added main script for scheme creation

---
+++ main_script.sql
@@ -0,0 +1,91 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema Polnyi_DB
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema Polnyi_DB
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Polnyi_DB` DEFAULT CHARACTER SET utf8 ;
+USE `Polnyi_DB` ;
+
+-- -----------------------------------------------------
+-- Table `Polnyi_DB`.`loader`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Polnyi_DB`.`loader` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `surname` VARCHAR(45) NOT NULL,
+  `mobile_phone` CHAR(10) NULL,
+  `company` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Polnyi_DB`.`snack_machine`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Polnyi_DB`.`snack_machine` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `adress` VARCHAR(50) NULL,
+  `last_load` DATE NULL,
+  `loader_id` INT NULL,
+  `last_cash_gathering` DATE NULL,
+  `gathered_cash` INT UNSIGNED ZEROFILL NULL,
+  `coins_load_date` DATE NULL,
+  `loaded_coins_sum` INT UNSIGNED ZEROFILL NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_snack_machine_loader_idx` (`loader_id` ASC) VISIBLE,
+  CONSTRAINT `fk_snack_machine_loader`
+    FOREIGN KEY (`loader_id`)
+    REFERENCES `Polnyi_DB`.`loader` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Polnyi_DB`.`snack`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Polnyi_DB`.`snack` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `trademark` VARCHAR(45) NOT NULL,
+  `snack_type` VARCHAR(45) NOT NULL,
+  `price` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Polnyi_DB`.`snack_machine_has_snack`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Polnyi_DB`.`snack_machine_has_snack` (
+  `snack_machine_id` INT NOT NULL,
+  `snack_id` INT NOT NULL,
+  `snack_amount` INT ZEROFILL UNSIGNED NULL,
+  `saled_snacks` INT ZEROFILL UNSIGNED NULL,
+  `loaded_snacks` INT ZEROFILL NULL,
+  PRIMARY KEY (`snack_machine_id`, `snack_id`),
+  INDEX `fk_snack_machine_has_snack_snack1_idx` (`snack_id` ASC) VISIBLE,
+  INDEX `fk_snack_machine_has_snack_snack_machine1_idx` (`snack_machine_id` ASC) VISIBLE,
+  CONSTRAINT `fk_snack_machine_has_snack_snack_machine1`
+    FOREIGN KEY (`snack_machine_id`)
+    REFERENCES `Polnyi_DB`.`snack_machine` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_snack_machine_has_snack_snack1`
+    FOREIGN KEY (`snack_id`)
+    REFERENCES `Polnyi_DB`.`snack` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
